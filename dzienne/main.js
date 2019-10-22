b8de48097ada530a9b9a0e8ae903cd52b61be816
kpodlaski <podlaski@uni.lodz.pl>
2019-10-22 13:00:07 +0200

    kulka z odbciem

--- dzienne/main.js
+++ dzienne/main.js
@@ -7,6 +7,9 @@ kulka = {
     vy:1,
 }
 
+// https://github.com/kpodlaski/
+//Repo: ProgGierUrzMob2019
+//Podkatalog dzienne
 function animation(){
     var canvas = document.getElementById('mycanvas');
     var ctx = canvas.getContext('2d');
@@ -18,6 +21,16 @@ function animation(){
     ctx.fill();
     kulka.x+=kulka.vx;
     kulka.y+=kulka.vy;
+    if (kulka.x<=0) {kulka.x=0; kulka.vx=-kula.vx }
+    if (kulka.x>=canvas.width) {
+        kulka.x=canvas.width; 
+        kulka.vx=-kulka.vx
+    }
+    if (kulka.y<=0) {kulka.y=0; kulka.vy=-kulka.vy; }
+    if (kulka.y>=canvas.height) {
+        kulka.y=canvas.height; 
+        kulka.vy=-kulka.vy 
+    }
     console.log(kulka.x, kulka.y, kulka.r);
     setTimeout(animation,40);
 }
