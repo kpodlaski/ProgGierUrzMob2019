0ac0e3e6834698cd30a03a7066ef42151240db35
kpodlaski <podlaski@uni.lodz.pl>
2019-10-26 16:26:38 +0200

    sprite object

---
+++ zaoczne/game_base/sprite_object.js
@@ -0,0 +1,26 @@
+class SpriteObject extends GameObject{
+
+    constructor(img, x, y, no_of_frames, timeout, w, h){
+        super(img,x,y,timeout);
+        this.no_of_frames = no_of_frames;
+        this.frame=0;
+        this.w = w;
+        this.h = h;
+    }
+
+    update(){
+        this.frame = (frame+1)%this.no_of_frames; 
+        this.x+=this.vx;
+        this.y+=this.vy;
+        setTimeout(this.update,this.timeout);
+    }
+
+    draw(ctx, width, height){
+        ctx.drawImage(this.img,
+            this.frame*this.img.width/this.no_of_frames,0,
+            this.img.width/this.no_of_frames,this.img.width,
+            this.x, this.y,
+            this.w, this.h);
+    }
+    
+}
