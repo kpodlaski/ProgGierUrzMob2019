75b9fdb190dd23429b8714ee1693dfbdee2e2890
kpodlaski <podlaski@uni.lodz.pl>
2019-10-26 14:54:15 +0200

    eagle

--- zaoczne/zaj1/animation.js
+++ zaoczne/zaj1/animation.js
@@ -6,11 +6,30 @@ var ctx = canvas.getContext("2d");
 bg_img = new Image();
 bg_img.src = "obrazki/scrolling_background.png";
 
+bird_img = new Image();
+bird_img.src = "bird.png";
+
+
 function animation(){
-    ctx.clearRect(0,0,canvas.width, canvas.hight);
-    //drawing bground image
-    ctx.drawImage(bg_img, bg_x, 0, 2*canvas.width, canvas.height);
+    offscreen = document.createElement("canvas");
+    offscreen.width = canvas.width;
+    offscreen.height = canvas.height;
+    offctx = offscreen.getContext("2d");
+    offctx.save();
+    offctx.drawImage(bg_img, bg_x, 0, 2*canvas.width, canvas.height);
+    offctx.translate(300, 100);
+    offctx.scale(-1,1); //odbicie w osi x
+    offctx.drawImage(bird_img,
+        frame*bird_img.width/4,
+        0,
+        bird_img.width/4, bird_img.height, 
+        50,100,
+        100,100);
+    offctx.restore();
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+    ctx.drawImage(offscreen,0,0,canvas.width,canvas.height);
     requestAnimationFrame(animation);
+
 }
 
 function move_background(){
@@ -20,10 +39,17 @@ function move_background(){
     }
     bg_x -=1;
     setTimeout(move_background,20);
-    console.log(bg_x);
+    //console.log(bg_x);
+}
+
+frame = 0;
+function move_bird(){
+    frame=(frame + 1)%4;
+    setTimeout(move_bird,60);
 }
 
 window.onload = function(){
     animation();
     move_background();
+    move_bird();
 }
